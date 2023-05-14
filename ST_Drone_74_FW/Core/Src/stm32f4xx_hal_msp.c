284b013388b6f7ec839179289c73577e1f0521c4
LONG-NHAN-HP <longnhan252@gmail.com>
2023-05-14 23:36:11 +0700

    reconfig project

--- ST_Drone_74_FW/Core/Src/stm32f4xx_hal_msp.c
+++ ST_Drone_74_FW/Core/Src/stm32f4xx_hal_msp.c
@@ -155,10 +155,9 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
 
     /**SPI2 GPIO Configuration
     PB13     ------> SPI2_SCK
-    PB14     ------> SPI2_MISO
     PB15     ------> SPI2_MOSI
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+    GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_15;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
@@ -210,10 +209,9 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef* hspi)
 
     /**SPI2 GPIO Configuration
     PB13     ------> SPI2_SCK
-    PB14     ------> SPI2_MISO
     PB15     ------> SPI2_MOSI
     */
-    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15);
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_13|GPIO_PIN_15);
 
     /* Peripheral interrupt DeInit*/
     HAL_NVIC_DisableIRQ(SPI2_IRQn);
