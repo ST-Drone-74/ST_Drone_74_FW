14545827a5d61b8fb1aac7d310cc781e1735c5c6
LONG-NHAN-HP <longnhan252@gmail.com>
2023-05-14 23:54:26 +0700

    SPI control function for magneto sensor

--- ST_Drone_74_FW/User_Drivers/mag_driver.h
+++ ST_Drone_74_FW/User_Drivers/mag_driver.h
@@ -8,6 +8,17 @@
 #ifndef MAG_DRIVER_H_
 #define MAG_DRIVER_H_
 
+#include "stm32f4xx_hal.h"
+#include "mxconstants.h"
 
+extern SPI_HandleTypeDef hspi2;
+
+/*COMPASS CONTROL FUNCTION*/
+void SPI_Read(SPI_HandleTypeDef* xSpiHandle, uint8_t *val);
+void SPI_Write(SPI_HandleTypeDef* xSpiHandle, uint8_t val);
+void SPI_Read_nBytes(SPI_HandleTypeDef* xSpiHandle, uint8_t *val, uint8_t size);
+
+void Compass_SPI_Read(SPI_HandleTypeDef* xSpiHandle, uint8_t ReadAddr, uint8_t *pBuffer, uint8_t size );
+void Compass_SPI_Write(SPI_HandleTypeDef* xSpiHandle, uint8_t WriteAddr, uint8_t *pBuffer, uint8_t size );
 
 #endif /* MAG_DRIVER_H_ */
