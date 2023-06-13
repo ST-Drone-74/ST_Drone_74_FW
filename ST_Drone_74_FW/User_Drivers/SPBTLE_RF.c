33e8ed4523fabace03df7e5868e60fbe20c5abdc
longnhan <longnhan252@gmail.com>
2023-06-13 22:55:28 +0700

    update ble fucntions
    - add ble init function
    - remove useless cmt
    -  organize ble source

--- ST_Drone_74_FW/User_Drivers/SPBTLE_RF.c
+++ ST_Drone_74_FW/User_Drivers/SPBTLE_RF.c
@@ -1,42 +1,9 @@
-/**
-******************************************************************************
-* @file    SBTLE_RF.c
-* @author  Competence Center Japan
-* @version V1.0.0
- * @date    05-Oct-2017
-* @brief   This file provides the BLE driver for the STEVAL_FCU001V1
-*          board (file derived from Sensortile board)
-******************************************************************************
-* @attention
-*
-* <h2><center>&copy; COPYRIGHT(c) 2014 STMicroelectronics</center></h2>
-*
-* Redistribution and use in source and binary forms, with or without modification,
-* are permitted provided that the following conditions are met:
-*   1. Redistributions of source code must retain the above copyright notice,
-*      this list of conditions and the following disclaimer.
-*   2. Redistributions in binary form must reproduce the above copyright notice,
-*      this list of conditions and the following disclaimer in the documentation
-*      and/or other materials provided with the distribution.
-*   3. Neither the name of STMicroelectronics nor the names of its contributors
-*      may be used to endorse or promote products derived from this software
-*      without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-******************************************************************************
-*/ 
-
-/* Includes ------------------------------------------------------------------*/
+/*
+ * SBTLE_RF.c
+ *
+ *  Created on: June 12, 2023
+ *      Author: Mai Huynh Long Nhan
+ */
 #include "SPBTLE_RF.h"
 #include "gp_timer.h"
 #include "debug.h"
@@ -46,57 +13,16 @@
 extern volatile uint32_t ms_counter;
 #endif /* PRINT_CSV_FORMAT */
 
-/** @addtogroup BSP
- *  @{
- */
-
-/** @addtogroup SENSORTILE
- *  @{
- */
- 
-/** @addtogroup SENSORTILE_BLE
-* @brief This file provides a set of firmware functions to manage BLE 
- *  @{
- */
-
-/** @defgroup SENSORTILE_BLE_Private_Defines SENSORTILE_BLE Private Defines
-* @{
-*/ 
-
 #define HEADER_SIZE 5
 #define MAX_BUFFER_SIZE 255
 #define TIMEOUT_DURATION 15
 
-/**
-* @}
-*/
-
-/** @defgroup SENSORTILE_BLE_Private_Variables SENSORTILE_BLE Private Variables
-* @{
-*/
-
 SPI_HandleTypeDef SpiHandle;
-
-/**
-* @}
-*/
-
-/** @defgroup SENSORTILE_BLE_Private_Function_Prototypes SENSORTILE_BLE Private Function Prototypes
-*  @{
-*/
 
 /* Private function prototypes -----------------------------------------------*/
 static void us150Delay(void);
 void set_irq_as_output(void);
 void set_irq_as_input(void);
-
-/**
-* @}
-*/ 
-
-/** @defgroup SENSORTILE_BLE_Exported_Functions SENSORTILE_BLE Exported Functions
-* @{
-*/ 
 
 #ifdef PRINT_CSV_FORMAT
 /**
@@ -117,117 +43,14 @@ void print_csv_time(void){
  */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {  
-  switch(GPIO_Pin){
+  switch(GPIO_Pin)
+  {
     case BNRG_SPI_EXTI_PIN:
       HCI_Isr();
       HCI_ProcessEvent=1;
     break;
   }
 }
-
-///**
-//* @brief  This function is used for low level initialization of the SPI 
-//*         communication with the BlueNRG Expansion Board.
-//* @param  hspi: SPI handle.
-//* @retval None
-//*/
-//void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
-//{
-//  GPIO_InitTypeDef GPIO_InitStruct;
-//  if(hspi->Instance==BNRG_SPI_INSTANCE)
-//  {
-//    /* Enable peripherals clock */
-// 
-//    
-//    /* Enable GPIO Ports Clock */  
-//    BNRG_SPI_RESET_CLK_ENABLE();
-//    BNRG_SPI_SCLK_CLK_ENABLE();
-//    BNRG_SPI_MISO_CLK_ENABLE();
-//    BNRG_SPI_MOSI_CLK_ENABLE();
-//    BNRG_SPI_CS_CLK_ENABLE();
-//    BNRG_SPI_IRQ_CLK_ENABLE();
-//    
-//    /* Enable SPI clock */
-//    BNRG_SPI_CLK_ENABLE();
-//    
-//    /* Reset */
-//    GPIO_InitStruct.Pin = BNRG_SPI_RESET_PIN;
-//    GPIO_InitStruct.Mode = BNRG_SPI_RESET_MODE;
-//    GPIO_InitStruct.Pull = BNRG_SPI_RESET_PULL;
-//    GPIO_InitStruct.Speed = BNRG_SPI_RESET_SPEED;
-//    GPIO_InitStruct.Alternate = BNRG_SPI_RESET_ALTERNATE;
-//    HAL_GPIO_Init(BNRG_SPI_RESET_PORT, &GPIO_InitStruct);	
-//    HAL_GPIO_WritePin(BNRG_SPI_RESET_PORT, BNRG_SPI_RESET_PIN, GPIO_PIN_RESET);	/*Added to avoid spurious interrupt from the BlueNRG */
-//    
-//    /* SCLK */
-//    GPIO_InitStruct.Pin = BNRG_SPI_SCLK_PIN;
-//    GPIO_InitStruct.Mode = BNRG_SPI_SCLK_MODE;
-//    GPIO_InitStruct.Pull = BNRG_SPI_SCLK_PULL;
-//    GPIO_InitStruct.Speed = BNRG_SPI_SCLK_SPEED;
-//    GPIO_InitStruct.Alternate = BNRG_SPI_SCLK_ALTERNATE;
-//    HAL_GPIO_Init(BNRG_SPI_SCLK_PORT, &GPIO_InitStruct); 
-//    
-//    /* MISO */
-//    GPIO_InitStruct.Pin = BNRG_SPI_MISO_PIN;
-//    GPIO_InitStruct.Mode = BNRG_SPI_MISO_MODE;
-//    GPIO_InitStruct.Pull = BNRG_SPI_MISO_PULL;
-//    GPIO_InitStruct.Speed = BNRG_SPI_MISO_SPEED;
-//    GPIO_InitStruct.Alternate = BNRG_SPI_MISO_ALTERNATE;
-//    HAL_GPIO_Init(BNRG_SPI_MISO_PORT, &GPIO_InitStruct);
-//    
-//    /* MOSI */
-//    GPIO_InitStruct.Pin = BNRG_SPI_MOSI_PIN;
-//    GPIO_InitStruct.Mode = BNRG_SPI_MOSI_MODE;
-//    GPIO_InitStruct.Pull = BNRG_SPI_MOSI_PULL;
-//    GPIO_InitStruct.Speed = BNRG_SPI_MOSI_SPEED;
-//    GPIO_InitStruct.Alternate = BNRG_SPI_MOSI_ALTERNATE;
-//    HAL_GPIO_Init(BNRG_SPI_MOSI_PORT, &GPIO_InitStruct);
-//    
-//    /* NSS/CSN/CS */
-//    GPIO_InitStruct.Pin = BNRG_SPI_CS_PIN;
-//    GPIO_InitStruct.Mode = BNRG_SPI_CS_MODE;
-//    GPIO_InitStruct.Pull = BNRG_SPI_CS_PULL;
-//    GPIO_InitStruct.Speed = BNRG_SPI_CS_SPEED;
-//    GPIO_InitStruct.Alternate = BNRG_SPI_CS_ALTERNATE;
-//    HAL_GPIO_Init(BNRG_SPI_CS_PORT, &GPIO_InitStruct);
-//    HAL_GPIO_WritePin(BNRG_SPI_CS_PORT, BNRG_SPI_CS_PIN, GPIO_PIN_SET);
-//    
-//    /* IRQ -- INPUT */
-//    GPIO_InitStruct.Pin = BNRG_SPI_IRQ_PIN;
-//    GPIO_InitStruct.Mode = BNRG_SPI_IRQ_MODE;
-//    GPIO_InitStruct.Pull = BNRG_SPI_IRQ_PULL;
-//    GPIO_InitStruct.Speed = BNRG_SPI_IRQ_SPEED;
-//    GPIO_InitStruct.Alternate = BNRG_SPI_IRQ_ALTERNATE;
-//    HAL_GPIO_Init(BNRG_SPI_IRQ_PORT, &GPIO_InitStruct);
-//    
-//    /* Configure the NVIC for SPI */  
-//    HAL_NVIC_SetPriority(BNRG_SPI_EXTI_IRQn, 3, 0);    
-//    HAL_NVIC_EnableIRQ(BNRG_SPI_EXTI_IRQn);
-//  }
-//  else if(hspi->Instance==SPI2)
-//  {
-//  /* USER CODE BEGIN SPI2_MspInit 0 */
-//
-//  /* USER CODE END SPI2_MspInit 0 */
-//    /* Peripheral clock enable */
-//    __HAL_RCC_SPI2_CLK_ENABLE();
-//  
-//    /**SPI2 GPIO Configuration    
-//    PB13     ------> SPI2_SCK
-//    PB15     ------> SPI2_MOSI 
-//    */
-//    GPIO_InitStruct.Pin = SPI2_CLK_Pin|SPI2_SDA_Pin;
-//    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-//    GPIO_InitStruct.Pull = GPIO_NOPULL;
-//    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-//    GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
-//    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-//
-//  /* USER CODE BEGIN SPI2_MspInit 1 */
-//
-//  /* USER CODE END SPI2_MspInit 1 */
-//  }
-//}
 
 /**
 * @brief  Writes data to a serial interface.
@@ -241,23 +64,26 @@ void Hal_Write_Serial(const void* data1, const void* data2, int32_t n_bytes1,
                       int32_t n_bytes2)
 {
   struct timer t;
-  
   Timer_Set(&t, CLOCK_SECOND/10);
   
 #ifdef PRINT_CSV_FORMAT
   print_csv_time();
-  for (int i=0; i<n_bytes1; i++) {
+  for (int i=0; i<n_bytes1; i++)
+  {
     PRINT_CSV(" %02x", ((uint8_t *)data1)[i]);
   }
-  for (int i=0; i<n_bytes2; i++) {
+  for (int i=0; i<n_bytes2; i++)
+  {
     PRINT_CSV(" %02x", ((uint8_t *)data2)[i]);
   }
   PRINT_CSV("\n");
 #endif
   
-  while(1){
+  while(1)
+  {
     if(BlueNRG_SPI_Write(&SpiHandle, (uint8_t *)data1,(uint8_t *)data2, n_bytes1, n_bytes2)==0) break;
-    if(Timer_Expired(&t)){
+    if(Timer_Expired(&t))
+    {
       break;
     }
   }
@@ -305,14 +131,13 @@ void BlueNRG_RST(void)
 * @param  None
 * @retval 1 if data are present, 0 otherwise
 */
-// FIXME: find a better way to handle this return value (bool type? TRUE and FALSE)
 uint8_t BlueNRG_DataPresent(void)
 {
   if (HAL_GPIO_ReadPin(BNRG_SPI_EXTI_PORT, BNRG_SPI_EXTI_PIN) == GPIO_PIN_SET)
     return 1;
   else  
     return 0;
-} /* end BlueNRG_DataPresent() */
+}
 
 /**
 * @brief  Activate internal bootloader using pin.
@@ -352,17 +177,21 @@ int32_t BlueNRG_SPI_Read_All(SPI_HandleTypeDef *hspi, uint8_t *buffer,
   /* Read the header */  
   HAL_SPI_TransmitReceive(hspi, header_master, header_slave, HEADER_SIZE, TIMEOUT_DURATION);
   
-  if (header_slave[0] == 0x02) {
+  if (header_slave[0] == 0x02)
+  {
     /* device is ready */
     byte_count = (header_slave[4]<<8)|header_slave[3];
-    if (byte_count > 0) {
+    if (byte_count > 0)
+    {
       
       /* avoid to read more data that size of the buffer */
-      if (byte_count > buff_size){
+      if (byte_count > buff_size)
+      {
         byte_count = buff_size;
       }
       
-      for (len = 0; len < byte_count; len++){
+      for (len = 0; len < byte_count; len++)
+      {
         __disable_irq();
         HAL_SPI_TransmitReceive(hspi, &char_ff, (uint8_t*)&read_char, 1, TIMEOUT_DURATION);
         __enable_irq();
@@ -378,9 +207,11 @@ int32_t BlueNRG_SPI_Read_All(SPI_HandleTypeDef *hspi, uint8_t *buffer,
   for(volatile int i = 0; i < 2; i++)__NOP();
   
 #ifdef PRINT_CSV_FORMAT
-  if (len > 0) {
+  if (len > 0)
+  {
     print_csv_time();
-    for (int i=0; i<len; i++) {
+    for (int i=0; i<len; i++)
+    {
       PRINT_CSV(" %02x", buffer[i]);
     }
     PRINT_CSV("\n");
@@ -403,7 +234,6 @@ int32_t BlueNRG_SPI_Write(SPI_HandleTypeDef *hspi, uint8_t* data1,
                           uint8_t* data2, uint8_t Nb_bytes1, uint8_t Nb_bytes2)
 {  
   int32_t result = 0;
-  
   int32_t spi_fix_enabled = 0;
   
 #ifdef ENABLE_SPI_FIX
@@ -412,7 +242,6 @@ int32_t BlueNRG_SPI_Write(SPI_HandleTypeDef *hspi, uint8_t* data1,
   
   unsigned char header_master[HEADER_SIZE] = {0x0a, 0x00, 0x00, 0x00, 0x00};
   unsigned char header_slave[HEADER_SIZE]  = {0xaa, 0x00, 0x00, 0x00, 0x00};
-  
   unsigned char read_char_buf[MAX_BUFFER_SIZE];
   
   Disable_SPI_IRQ(); 
@@ -423,7 +252,8 @@ int32_t BlueNRG_SPI_Write(SPI_HandleTypeDef *hspi, uint8_t* data1,
   header transmit/receive operations are started.
   After these transmit/receive operations the IRQ is reset in input mode.
   */
-  if (spi_fix_enabled) {
+  if (spi_fix_enabled)
+  {
     set_irq_as_output();
     
     /* Assert CS line after at least 112us */
@@ -438,43 +268,47 @@ int32_t BlueNRG_SPI_Write(SPI_HandleTypeDef *hspi, uint8_t* data1,
   HAL_SPI_TransmitReceive(hspi, header_master, header_slave, HEADER_SIZE, TIMEOUT_DURATION);
   __enable_irq();
   
-  if (spi_fix_enabled) {
+  if (spi_fix_enabled)
+  {
     set_irq_as_input();
   }
   
-  if (header_slave[0] == 0x02) {
+  if (header_slave[0] == 0x02)
+  {
     /* SPI is ready */
-    if (header_slave[1] >= (Nb_bytes1+Nb_bytes2)) {
+    if (header_slave[1] >= (Nb_bytes1+Nb_bytes2))
+    {
       
       /*  Buffer is big enough */
-      if (Nb_bytes1 > 0) {
+      if (Nb_bytes1 > 0)
+      {
         __disable_irq();
         HAL_SPI_TransmitReceive(hspi, data1, read_char_buf, Nb_bytes1, TIMEOUT_DURATION);
         __enable_irq();
-        
       }
-      if (Nb_bytes2 > 0) {
+      if (Nb_bytes2 > 0)
+      {
         __disable_irq();
         HAL_SPI_TransmitReceive(hspi, data2, read_char_buf, Nb_bytes2, TIMEOUT_DURATION);
         __enable_irq();
-        
       }
       
-    } else {
+    }
+    else
+    {
       /* Buffer is too small */
       result = -2;
     }
-  } else {
+  }
+  else
+  {
     /* SPI is not ready */
     result = -1;
   }
   
   /* Release CS line */
   HAL_GPIO_WritePin(BNRG_SPI_CS_PORT, BNRG_SPI_CS_PIN, GPIO_PIN_SET);
-  
-  
   Enable_SPI_IRQ();
-  
   return result;
 }
 
@@ -580,20 +414,117 @@ void Clear_SPI_EXTI_Flag(void)
   __HAL_GPIO_EXTI_CLEAR_IT(BNRG_SPI_EXTI_PIN);  
 }
 
-/**
-* @}
-*/
+void BlueNRG_Init(void)
+{
 
-/**
-* @}
-*/
+  int ret = 1;
+  uint8_t bdaddr[6];
+  uint8_t  hwVersion=0;
+  uint16_t fwVersion=0;
+  uint16_t service_handle, dev_name_char_handle, appearance_char_handle;
+  DrvStatusTypeDef testStatus;
 
-/**
-* @}
-*/
+  PRINTF("****** START BLE TESTS ******\r\n");
+  BNRG_SPI_Init();
 
-/**
-* @}
-*/
+  bdaddr[0] = (STM32_UUID[1]>>24)&0xFF;
+  bdaddr[1] = (STM32_UUID[0]    )&0xFF;
+  bdaddr[2] = (STM32_UUID[2] >>8)&0xFF;
+  bdaddr[3] = (STM32_UUID[0]>>16)&0xFF;
+  bdaddr[4] = (hwVersion > 0x30) ?
+            ((((0x34-48)*10) + (0x30-48)+100)&0xFF) :
+            ((((0x34-48)*10) + (0x30-48)    )&0xFF) ;
+  bdaddr[5] = 0xC0; /* for a Legal BLE Random MAC */
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+  /* Initialize the BlueNRG HCI */
+  HCI_Init();
+
+ /* Reset BlueNRG hardware */
+  BlueNRG_RST();
+
+  /* get the BlueNRG HW and FW versions */
+  PRINTF("\r\nReading BlueNRG version ...\r\n");
+  if (getBlueNRGVersion(&hwVersion, &fwVersion)== BLE_STATUS_SUCCESS)
+  {
+     //Reset BlueNRG again otherwise it will fail.
+    BlueNRG_RST();
+
+    PRINTF("GATT Initializzation...\r\n");
+    ret = aci_gatt_init();
+    if(ret)
+    {
+      testStatus = COMPONENT_ERROR;
+      PRINTF("\r\nGATT_Init failed ****\r\n");
+      goto fail;
+    }
+    //Set the GAP INIT like X-NUCLEO-IDB05A1 eval board  since using same SPBTLE_RF module
+    ret = aci_gap_init_IDB05A1(GAP_PERIPHERAL_ROLE_IDB05A1, 0, 0x07, &service_handle, &dev_name_char_handle, &appearance_char_handle);
+
+    if(ret != BLE_STATUS_SUCCESS)
+    {
+      PRINTF("\r\nGAP_Init failed\r\n");
+      goto fail;
+    }
+    ret = hci_le_set_random_address(bdaddr);
+    const char BoardName[7] = {NAME_BLUEMS};
+    ret = aci_gatt_update_char_value(service_handle, dev_name_char_handle, 0,
+                                       7/*strlen(BoardName)*/, (uint8_t *)BoardName);
+
+    PRINTF("GAP setting Authentication ....\r\n");
+    ret = aci_gap_set_auth_requirement(MITM_PROTECTION_REQUIRED,
+                                       OOB_AUTH_DATA_ABSENT,
+                                       NULL, 7, 16,
+                                       USE_FIXED_PIN_FOR_PAIRING, 123456,
+                                       BONDING);
+    if (ret != BLE_STATUS_SUCCESS)
+    {
+      testStatus = COMPONENT_ERROR;
+       PRINTF("\r\nGAP setting Authentication failed ******\r\n");
+       goto fail;
+    }
+
+    PRINTF("SERVER: BLE Stack Initialized \r\n"
+           "Board HWver=%d, FWver=%d.%d.%c\r\n"
+           "BoardMAC = %x:%x:%x:%x:%x:%x\r\n",
+           hwVersion,
+           fwVersion>>8,
+           (fwVersion>>4)&0xF,
+           (hwVersion > 0x30) ? ('a'+(fwVersion&0xF)-1) : 'a',
+           bdaddr[5],bdaddr[4],bdaddr[3],bdaddr[2],bdaddr[1],bdaddr[0]);
+
+    /* Set output power level */
+    aci_hal_set_tx_power_level(1,4);    /* -2.1dBm */
+
+    ret = Add_ConsoleW2ST_Service();
+    if(ret == BLE_STATUS_SUCCESS)
+    {
+       PRINTF("Console Service W2ST added successfully\r\n");
+    }
+    else
+    {
+       testStatus = COMPONENT_ERROR;
+       PRINTF("\r\nError while adding Console Service W2ST\r\n");
+    }
+
+    ret = Add_ConfigW2ST_Service();
+    if(ret == BLE_STATUS_SUCCESS)
+       PRINTF("Config  Service W2ST added successfully\r\n");
+    else{
+       testStatus = COMPONENT_ERROR;
+       PRINTF("\r\nError while adding Config Service W2ST\r\n");
+    }
+
+    PRINTF("\r\nAll test passed!\r\n");
+  }
+  else {
+       testStatus = COMPONENT_ERROR;
+       PRINTF("\r\nError in BlueNRG tests. ******\r\n");
+  }
+  PRINTF("****** END BLE TESTS ******\r\n");
+  return;
+
+fail:
+  testStatus = COMPONENT_ERROR;
+  return;
+}
+
--- ST_Drone_74_FW/User_Drivers/SPBTLE_RF.h
+++ ST_Drone_74_FW/User_Drivers/SPBTLE_RF.h
@@ -1,41 +1,10 @@
-/**
-  ******************************************************************************
-  * @file    SensorTile_BlueNRG.h
-  * @author  Central Labs
-  * @version V1.2.0
- * @date    10-Nov-2016
-  * @brief   This file contains definitions for the SensorTile_BlueNRG.c
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT(c) 2014 STMicroelectronics</center></h2>
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */ 
-  
-/* Define to prevent recursive inclusion -------------------------------------*/
+/*
+ * SBTLE_RF.h
+ *
+ *  Created on: June 12, 2023
+ *      Author: Mai Huynh Long Nhan
+ */
+
 #ifndef __STM32_BLUENRG_BLE_H
 #define __STM32_BLUENRG_BLE_H
 
@@ -45,28 +14,19 @@
 
 /* Includes ------------------------------------------------------------------*/ 
 #include "stm32f4xx_hal.h"
-#define SYSCLK_FREQ 80000000
 #include <mxconstants.h>
- extern uint32_t HCI_ProcessEvent;
-/** @addtogroup BSP
- *  @{
- */
+#include "ble_status.h"
+#include "bluenrg_gatt_server.h"
+#include "bluenrg_utils.h"
+#include "sm.h"
+#include "bluenrg_l2cap_aci.h"
 
-/** @addtogroup SENSORTILE
- *  @{
- */
- 
-/** @addtogroup SENSORTILE_BLE
- *  @{
- */
-
-/** @defgroup SENSORTILE_BLE_Exported_Constants SENSORTILE_BLE Exported Constants
-  * @{
-  */   
+extern uint32_t HCI_ProcessEvent;
 
 // SPI Instance
 #define BNRG_SPI_INSTANCE           SPI1
 #define BNRG_SPI_CLK_ENABLE()       __SPI1_CLK_ENABLE()
+#define SYSCLK_FREQ 80000000
 
 // SPI Configuration
 #define BNRG_SPI_MODE               SPI_MODE_MASTER
@@ -139,68 +99,47 @@
 #define BNRG_SPI_IRQ_PORT           GPIOA
 #define BNRG_SPI_IRQ_CLK_ENABLE()   __GPIOA_CLK_ENABLE()
 
-// EXTI External Interrupt for SPI
-// NOTE: if you change the IRQ pin remember to implement a corresponding handler
-// function like EXTI0_IRQHandler() in the user project
 #define BNRG_SPI_EXTI_IRQn          EXTI4_IRQn
-//#define BNRG_SPI_EXTI_IRQHandler    EXTI4_IRQHandler // Forse SPI1_IRQHandler?
 #define BNRG_SPI_EXTI_PIN           BNRG_SPI_IRQ_PIN
 #define BNRG_SPI_EXTI_PORT          BNRG_SPI_IRQ_PORT
-//#define RTC_WAKEUP_IRQHandler       RTC_WKUP_IRQHandler
 
-   /**
-  * @}
-  */
 
-/** @defgroup SENSORTILE_BLE_Exported_Functions SENSORTILE_BLE Exported Functions
-  * @{
-  */
-  
-extern void Enable_SPI_IRQ(void);
-extern void Disable_SPI_IRQ(void);
-extern void Clear_SPI_IRQ(void);
-extern void Clear_SPI_EXTI_Flag(void);
+ typedef enum
+ {
+   COMPONENT_OK = 0,
+   COMPONENT_ERROR,
+   COMPONENT_TIMEOUT,
+   COMPONENT_NOT_IMPLEMENTED
+ } DrvStatusTypeDef;
 
-// FIXME: add prototypes for BlueNRG here
-extern void BNRG_SPI_Init(void);
-extern void BlueNRG_RST(void);
-extern uint8_t BlueNRG_DataPresent(void);
-extern void    BlueNRG_HW_Bootloader(void);
-extern int32_t BlueNRG_SPI_Read_All(SPI_HandleTypeDef *hspi,
+void Enable_SPI_IRQ(void);
+void Disable_SPI_IRQ(void);
+void Clear_SPI_IRQ(void);
+void Clear_SPI_EXTI_Flag(void);
+
+void BNRG_SPI_Init(void);
+void BlueNRG_RST(void);
+uint8_t BlueNRG_DataPresent(void);
+void    BlueNRG_HW_Bootloader(void);
+int32_t BlueNRG_SPI_Read_All(SPI_HandleTypeDef *hspi,
                              uint8_t *buffer,
                              uint8_t buff_size);
-extern int32_t BlueNRG_SPI_Write(SPI_HandleTypeDef *hspi,
+int32_t BlueNRG_SPI_Write(SPI_HandleTypeDef *hspi,
                           uint8_t* data1,
                           uint8_t* data2,
                           uint8_t Nb_bytes1,
                           uint8_t Nb_bytes2);
 
-extern void Hal_Write_Serial(const void* data1, const void* data2, int32_t n_bytes1,
+void Hal_Write_Serial(const void* data1, const void* data2, int32_t n_bytes1,
                       int32_t n_bytes2);
 
-extern void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi);
-extern void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
-/**
- * @}
- */
+void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi);
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
+void BlueNRG_Init(void);
 
-/**
- * @}
- */
-
-/**
- * @}
- */
- 
-/**
- * @}
- */
- 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* __STM32_BLUENRG_BLE_H */
-    
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
 
